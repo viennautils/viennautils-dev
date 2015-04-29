aae5eb521fc40d3d88a4353319b58cf0713bbcf4
Andreas Morhammer <morhammer@iue.tuwien.ac.at>
2015-04-29 12:29:02 +0200

    added extract_stem and extract_extension functions to filesystem

--- include/viennautils/filesystem/filesystem.hpp
+++ include/viennautils/filesystem/filesystem.hpp
@@ -5,10 +5,15 @@
 
 namespace viennautils
 {
+namespace filesystem
+{
 
+std::string extract_extension(std::string const & path, bool include_delimiter = false);
+std::string extract_stem(std::string const & path);
 std::string extract_filename(std::string const & path);
-std::string extract_path(std::string const & path);
+std::string extract_path(std::string const & path, bool include_last_delimiter = false);
 
+} //end of namespace filesystem
 } //end of namespace viennautils
 
 #endif
